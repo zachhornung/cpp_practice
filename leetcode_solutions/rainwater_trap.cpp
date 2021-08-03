afe6960b881e7980669789015a196ca19707f4db
Zach Hornung <Zachhornung@comcast.net>
2021-08-02 23:24:56 -0700

    added rainwater trap leetcode solution

---
+++ leetcode_solutions/rainwater_trap.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+        int ans = 0;
+        int l_max = 0, r_max = 0;
+        while (l < r){
+            if (height[l] < height[r]){
+                if (height[l] >= l_max){
+                    l_max = height[l];
+                }else {
+                    ans += (l_max - height[l]);
+                }
+                ++l;
+            }else {
+                if (height[r] >= r_max){
+                    r_max = height[r];
+                }else {
+                    ans += (r_max - height[r]);
+                }
+                --r;
+            }
+        }
+        return ans;
+    }
+};
